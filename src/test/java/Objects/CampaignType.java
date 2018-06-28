783149d3b6555475a74eb6423533fbd0304bfdfc
Duy <nguyenvinhduydn@gmal.com>
2018-06-28 19:58:30 +0700

    Duy add campaign

---
+++ src/test/java/Objects/CampaignType.java
@@ -0,0 +1,13 @@
+package Objects;
+
+public class CampaignType {
+    private String Name;
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+}
